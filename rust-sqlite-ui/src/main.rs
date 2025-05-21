5c2b69935965bddf63baca832f93ddb23ebd71f2
ramses2099 <ramses2099@gmail.com>
2025-05-21 13:46:23 -0400

    sqlite with rust

--- rust-sqlite-ui/src/main.rs
+++ rust-sqlite-ui/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+use rusqlite::{ Connection, Result };
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // -- Memory SQLite
+    let conn = Connection::open_in_memory()?;
+    // -- Create Schema
+    conn.execute(
+        "CREATE TABLE person (
+            id   INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            data BLOB
+        ) STRICT",
+        ()
+    )?;
+    // -- Insert
+    // OK in `strict` mode
+    let p = Person {
+        id: 0,
+        name: "Jean".to_string(),
+        data: None,
+    };
+    conn.execute("INSERT INTO person(name,data)
+        VALUES(?1,?2)", (&p.name, &p.data))?;
+
+    // -- Select
+    let mut stmt = conn.prepare("
+    SELECT id, name, data FROM person")?;
+    let mut rows = stmt.query([])?;
+
+    while let Some(row) = rows.next()? {
+        let name: String = row.get(1)?;
+        println!("-->> name: {}", name);
+        println!("-->> row: {:?}", row);
+    }
+
+    // -- Print table
+    pretty_sqlite::print_table(&conn, "person")?;
+
+    Ok(())
 }
